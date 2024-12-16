59cd0673177d425a93ef51e635c73149b38c4eba
lucascodebr20 <lucascode@gmail.com>
2024-12-16 20:01:07 -0300

    Desafio Entrevista

--- src/main/java/br/com/ada/estacionamento/EstacionamentoApplication.java
+++ src/main/java/br/com/ada/estacionamento/EstacionamentoApplication.java
@@ -10,6 +10,4 @@ public class EstacionamentoApplication {
 		SpringApplication.run(EstacionamentoApplication.class, args);
 	}
 
-
-
 }
--- src/main/java/br/com/ada/estacionamento/carros/CarroService.java
+++ src/main/java/br/com/ada/estacionamento/carros/CarroService.java
@@ -1,20 +1,46 @@
 package br.com.ada.estacionamento.carros;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.ada.estacionamento.vagas.Vaga;
+import br.com.ada.estacionamento.vagas.VagaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarroService {
 
 
     public CarroRepository carroRepository;
+    public VagaRepository vagaRepository;
+
 
     public void cadastrarCarro(Carro carro){
         carroRepository.save(carro);
     }
 
     public void estacionar(Carro carro){
-        carroRepository.save(carro);
-    }
+        List<Vaga> vagaList = (List<Vaga>) vagaRepository.findAll();
+        int contador = 0;
+        
+        for (int i =0; i < vagaList.size(); i++) {
 
+            if (vagaList.get(i).isOcupada()) {
+                contador++;
+                System.out.println("Vaga Ocupada");
+            }
+
+            if (contador == 10) {
+                System.out.println("Estacionamento todo ocupado");
+            } else {
+                if (carro.getVaga().isOcupada() == false) {
+                    carro.getVaga().setOcupada(true);
+                    carroRepository.save(carro);
+                    System.out.println("Carro estacionado");
+                    break;
+                } else {
+                    System.out.println("Vaga já esta ocupada");
+                }
+            }
+        }
+    }
 }
